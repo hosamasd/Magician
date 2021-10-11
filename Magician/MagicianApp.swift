0caa726b6e7a281309f5ba981563919fe3c91827
hosam <hosamelmalt@gmail.com>
2021-10-11 13:56:59 +0200

    Initial Commit

---
+++ Magician/MagicianApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicianApp.swift
+//  Magician
+//
+//  Created by hosam on 11/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct MagicianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
